bd243e31cdb6367125ce2f7acabceeb9d7ef598c
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-07-09 00:19:14 +0530

    Add mongoose DB logic & userModel logic

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
 const express = require("express");
+const { connectDB } = require("./config/database");
+const { userModel } = require("./models/user");
 const app = express();
 
-const { adminAuth } = require("./middlewares/auth");
+// post api call to add data in the database
+app.post("/signup", async (req, res) => {
+  
+  // creating a new instance of the userModel
+  const user = new userModel({
+    firstName: "Shailesh",
+    lastName: "Singh",
+    email: "shaileshkumarsingh988@gmail.com",
+    password: "shailesh@123",
+  });
 
-app.get("/user", (req, res) => {
-  res.send("user data sent");
+  user.save();
+  res.send("user data added successfully");
 });
 
-app.get("/admin/getAllData", adminAuth, (req, res) => {
-  res.send("All Data Sent");
-});
-
-app.get("/admin/deleteData", adminAuth, (req, res) => {
-  res.send("Deleted Data Successfully");
-});
-
-app.listen(7777, () => {
-  console.log("Port is running successfully");
-});
+connectDB()
+  .then(() => {
+    console.log("DB Successfully connected");
+    app.listen(7777, () => {
+      console.log("Server is running successfully on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("DB was not connected");
+  });
