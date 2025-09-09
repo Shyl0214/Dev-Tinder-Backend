0bc59fb6ed23976886c991b887689d3b14dd51e1
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-09 11:16:56 +0530

    Defined basic routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("hello from root url");
+app.use("/user", (req, res) => {
+  res.send("hello hello hello");
 });
 
-app.use("/test", (req, res) => {
-  res.send("hello from test");
+app.get("/user", (req, res) => {
+  res.send("hello from the user");
 });
+
+app.post("/user", (req, res) => {
+  res.send("data added successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("user updated successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully");
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server running successfully");
