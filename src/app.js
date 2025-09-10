f96df0b6589f4a5cbdd3da3dee169a4f34830564
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-10 15:45:34 +0530

    connected to DB

--- src/app.js
+++ src/app.js
@@ -1,33 +1,16 @@
 const express = require("express");
 const { authAdmin, userAuth } = require("./middleware/auth");
+const { connectDB } = require("./config/database");
 
 const app = express();
 
-//middle-ware
-app.use("/admin", authAdmin);
-
-// admin routes
-app.get("/admin/allUserData", (req, res) => {
-  res.send("Data sent to admin");
-});
-
-app.delete("/admin/delUserData", (req, res) => {
-  res.send("Deleted user data");
-});
-
-// user routes
-app.get("/user/getProfileData", userAuth, (req, res) => {
-  res.send("Profile data sent");
-});
-
-app.delete("/user/delProfileData", userAuth, (req, res) => {
-  res.send("Profile data deleted");
-});
-
-app.post("/user/login", (req, res) => {
-  res.send("user logged in");
-});
-
-app.listen(3000, () => {
-  console.log("Server running successfully");
-});
+connectDB()
+  .then(() => {
+    console.log("DB Successfully Connected");
+    app.listen(3000, () => {
+      console.log("Server running successfully");
+    });
+  })
+  .catch(() => {
+    console.log("DB Connection Failed");
+  });
