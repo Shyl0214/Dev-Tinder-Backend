63cb32234022d67b1d212b9d59e395b749f5ed3a
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-10 18:57:30 +0530

    post user route logic implemented

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const { authAdmin, userAuth } = require("./middleware/auth");
 const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Smith",
+    lastName: "roy",
+    email: "smith12@gmail.com",
+    password: "sm123",
+  });
+
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(400).send("Error adding user");
+  }
+});
 
 connectDB()
   .then(() => {
