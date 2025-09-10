9079febee50cf9b21c74dad961433430f803f026
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-10 12:35:10 +0530

    user auth logic implemented

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,6 +8,17 @@ const authAdmin = (req, res, next) => {
   }
 };
 
+const userAuth = (req, res,next) => {
+  const token = "user";
+  const isAuth = token === "user";
+  if (!isAuth) {
+    res.status(400).send("Not Authorized");
+  }else{
+    next();
+  }
+};
+
 module.exports = {
   authAdmin,
+  userAuth,
 };
