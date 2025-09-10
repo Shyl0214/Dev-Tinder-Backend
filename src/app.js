f8587b36f532259a3ec8aaac05a65dc352118e90
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-10 12:34:39 +0530

    dummy user auth middleware added

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const express = require("express");
-const { authAdmin } = require("./middleware/auth");
+const { authAdmin, userAuth } = require("./middleware/auth");
 
 const app = express();
 
 //middle-ware
 app.use("/admin", authAdmin);
 
+// admin routes
 app.get("/admin/allUserData", (req, res) => {
   res.send("Data sent to admin");
 });
 
 app.delete("/admin/delUserData", (req, res) => {
   res.send("Deleted user data");
+});
+
+// user routes
+app.get("/user/getProfileData", userAuth, (req, res) => {
+  res.send("Profile data sent");
+});
+
+app.delete("/user/delProfileData", userAuth, (req, res) => {
+  res.send("Profile data deleted");
+});
+
+app.post("/user/login", (req, res) => {
+  res.send("user logged in");
 });
 
 app.listen(3000, () => {
